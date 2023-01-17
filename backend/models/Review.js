eff4919baf7b2ea0dbfa62dff45e1f4174074a73
Bikramjit Singh <singhbikramjit222@gmail.com>
2023-01-16 16:31:37 -0800

    Schema and validation done for reviews

---
+++ backend/models/Review.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    reviewerId:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        index: true
+    },
+    postId:{
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+        index: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = Review = mongoose.model("Review", ReviewSchema);
