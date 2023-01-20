972beec5736b79ff71dc45c4fb7df4ac9df8a495
Nestor Vega <vega.nestor1@gmail.com>
2023-01-19 16:12:11 -0800

    frontend hosting

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,29 @@ if (!isProduction) {
     app.use(cors());
 }
 
+
+if (isProduction) {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    app.get('/', (req, res) => {
+      res.cookie('CSRF-TOKEN', req.csrfToken());
+      res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      );
+    });
+
+    // Serve the static assets in the frontend's build folder
+    app.use(express.static(path.resolve("../frontend/build")));
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    app.get(/^(?!\/?api).*/, (req, res) => {
+      res.cookie('CSRF-TOKEN', req.csrfToken());
+      res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      );
+    });
+  }
+
 //setup csrf token
 app.use(csurf({
     cookie: {
