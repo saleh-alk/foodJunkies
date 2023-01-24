612cfd7ef90b593ba84d3c4d8c628c40236aab88
Saleh Alkaheli <skalkaheli@gmail.com>
2023-01-23 22:04:45 -0800

    debug

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,7 @@ if (isProduction) {
     const path = require('path');
     // Serve the frontend's index.html file at the root route
     app.get('/', (req, res) => {
-      res.cookie('CSRF-TOKEN', req.csrfToken());
+      res.cookie('CSRF-Token', req.csrfToken());
       res.sendFile(
         path.resolve(__dirname, '../frontend', 'build', 'index.html')
       );
@@ -62,7 +62,7 @@ if (isProduction) {
 
     // Serve the frontend's index.html file at all other routes NOT starting with /api
     app.get(/^(?!\/?api).*/, (req, res) => {
-      res.cookie('CSRF-TOKEN', req.csrfToken());
+      res.cookie('CSRF-Token', req.csrfToken());
       res.sendFile(
         path.resolve(__dirname, '../frontend', 'build', 'index.html')
       );
